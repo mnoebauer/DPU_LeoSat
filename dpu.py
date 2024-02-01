9a6483d2b1fecff9428fc4be34796cc52141d7aa
Manuel <manuel2@noebauer.at>
2024-02-01 19:24:24 +0100

    modified:   dpu.py

--- dpu.py
+++ dpu.py
@@ -7,7 +7,7 @@ def initRTC():
     global rtc
     i2c = board.I2C()
     rtc = adafruit_ds3231.DS3231(i2c)
-    rtc.datetime = time.struct_time((0000,0,0,0,0,0,0,1,-1))
+    rtc.datetime = time.struct_time((2024,0,0,0,0,0,0,1,-1))
 
 def writeTextToLog(a):
     t = rtc.datetime
