dd83dfc9d0012a467391c206695af8a23fccbe1f
Manuel <manuel2@noebauer.at>
2024-02-01 19:27:18 +0100

    modified:   dpu.py

--- dpu.py
+++ dpu.py
@@ -20,7 +20,7 @@ def main():
     #writeTextToLog('Starting:     Boot')
     initRTC()
     for i in range(10):
-        writeTextToLog(str(1))
+        writeTextToLog(" Test Test")
         time.sleep(2)
          
 if __name__ == "__main__":
