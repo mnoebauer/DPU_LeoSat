8680e7e18e4abcded4c748df5e30ca64bd13d899
Manuel <manuel2@noebauer.at>
2024-02-01 19:59:17 +0100

    modified:   dpu.py

--- dpu.py
+++ dpu.py
@@ -4,11 +4,23 @@ import bme680
 import adafruit_ds3231
 
 errorcounter = 0
+data = []
+
+def readAll():
+    writeTextToLog('Starting:       Reading Sensor Data')
+    try:
+        readBME688()
+    except:
+        writeTextToLog('Failed:       Reading Sensor Data')
+        errorcounter += 1
+    finally:
+        writeTextToLog('Starting:       Reading Sensor Data')
 
 def initializeAll():
     writeTextToLog('Starting:       Initialisation of Sensors')
     try:
         initBME688()
+
     except:
         writeTextToLog('Failed:     Initialisation of Sensors')
         errorCounter += 1
@@ -19,7 +31,7 @@ def initBME688():
     writeTextToLog('Starting:       Initialisation BME688')
     global sensor_bme680
     try:
-        bme688 = bme680.BME680(bme680.I2C_ADDR_SECONDARY)    
+        bme688 = bme680.BME680(bme680.I2C_ADDR_SECONDARY)   #initializing the bme688 with the secondary i2c adress
         bme688.set_humidity_oversample(bme680.OS_8X)
         bme688.set_pressure_oversample(bme680.OS_8X)
         bme688.set_temperature_oversample(bme680.OS_8X)
@@ -29,6 +41,20 @@ def initBME688():
         errorCounter += 1
     else:
         writeTextToLog("Success:        Initialisation BME688")
+
+def readBME688():
+    writeTextToLog('Starting:       Reading BME688')
+    try:
+        if sensor_bme680.get_sensor_data():
+            temperatur = sensor_bme680.data.temperature
+            pressure = sensor_bme680.data.pressure
+            humidity = sensor_bme680.data.humidity
+            data.append(temperatur,pressure,humidity)
+    except:
+        writeTextToLog('Failed:      Reading BME688')
+        errorCounter += 1
+    finally:
+        writeTextToLog('Success:        Reading BME688')
 
 def initRTC():
     global rtc #setting rtc variable global so it can be used everywhere
@@ -48,7 +74,7 @@ def main():
     writeTextToLog('Succesfull:     Initializing RTC')
     writeTextToLog('Starting:     Boot')
     initializeAll()
-
+    readAll()
          
 if __name__ == "__main__":
     main()
