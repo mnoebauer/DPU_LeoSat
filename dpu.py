4329896e7d18eb491430edbd5e89cec1a3cbe2f7
Manuel <manuel2@noebauer.at>
2024-02-01 20:28:39 +0100

    modified:   dpu.py

--- dpu.py
+++ dpu.py
@@ -8,11 +8,13 @@ data = []
 def readAll():
     global errorcounter
     writeTextToLog('Starting:       Reading Sensor Data')
+    try:
+        readBME688()
+    except:
+        writeTextToLog('Failed:       Reading Sensor Data')
 
-    readBME688()
-
-
-
+    finally:
+        writeTextToLog('Starting:       Reading Sensor Data')
 
 def initializeAll():
     global errorcounter
@@ -42,8 +44,8 @@ def initBME688():
         writeTextToLog("Success:        Initialisation BME688")
 
 def readBME688():
-    global temperatur, pressure, humidity
-    writeTextToLog('Starting:       Reading Sensor Data')
+    global errorcounter, data
+    writeTextToLog('Starting:       Reading BME688')
     try:
         if sensor_bme680.get_sensor_data():
             temperatur = sensor_bme680.data.temperature
@@ -51,10 +53,10 @@ def readBME688():
             humidity = sensor_bme680.data.humidity
             data.append(temperatur,pressure,humidity)
     except:
-        writeTextToLog('Failed:      Reading Sensor Data')
+        writeTextToLog('Failed:      Reading BME688')
 
     finally:
-        writeTextToLog('Success:        Reading Sensor Data')
+        writeTextToLog('Success:        Reading BME688')
 
 def initRTC():
     global rtc #setting rtc variable global so it can be used everywhere
@@ -74,11 +76,7 @@ def main():
     writeTextToLog('Succesfull:     Initializing RTC')
     writeTextToLog('Starting:     Boot')
     initializeAll()
-    i = 0
-    for i in range(10):
-        readAll()
-        writeTextToLog(str(data))
-        i += 1
+    readBME688()
          
 if __name__ == "__main__":
     main()
