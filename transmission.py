e246d525508dec26922e0aaa9baeea9c171f2826
Manuel <manuel2@noebauer.at>
2024-02-06 18:19:19 +0100

    modified:   transmission.py

--- transmission.py
+++ transmission.py
@@ -8,6 +8,14 @@ class Transmission:
         """
         send function
         """
+        activateCom()
+
+        if(waitForResponse() == True):
+            #response is there, ready to transmit data
+            print("True")
+        else:
+            print("No respones, not transmitting")
+
 
 async def activateCom():
     GPIO.setmode(GPIO.Board)
@@ -21,3 +29,14 @@ async def waitForResponse():
     GPIO.setmode(GPIO.Board)
     GPIO.setup(27,GPIO.IN)
 
+    channel = GPIO.wait_for_edge(27, GPIO_RISING, timeout = 5000)
+    await asyncio.sleep(3)
+    if channel is None:
+        r = False
+        print("Timeout occured")
+    else:
+        r = True
+        print("Edge detected on channel")
+    return r
+    
+        
