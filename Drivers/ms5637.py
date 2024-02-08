b464f3820ea390bb754b5bcb1dfd83c242a04ee1
Manuel <manuel2@noebauer.at>
2024-02-08 13:13:00 +0100

    modified:   Drivers/ms5637.py 	modified:   flightLogic.py

--- Drivers/ms5637.py
+++ Drivers/ms5637.py
@@ -2,7 +2,8 @@ from PiicoDev_MS5637 import PiicoDev_MS5637
 
 class ms5637():
     """
-    Driver for the MS5637 altitude and pressure sensors
+    Driver for the MS5637 altitude and pressure sensor, 
+    but only the altitude is used
     """
     def read():
         sensor = PiicoDev_MS5637()
--- flightLogic.py
+++ flightLogic.py
@@ -1,8 +1,6 @@
 import asyncio
-from Drivers import ms5637, bma400
-from Drivers import heartbeat
+from Drivers import heartbeat, ms5637
 import getSensorData
-import transmission
 
 def __main__():
     asyncio.run((mainFlightLogic()))
@@ -13,9 +11,18 @@ async def mainFlightLogic():
     It constantly runs the heartbeat to show the watchdog that it is alive
     It constantly gets the sensor data and saves it
     """
+    bootLogic() #running boot logic to update the reference values
 
     highPriorityTasks = []
     highPriorityTasks.append(asyncio.create_task(heartbeat.heartbeart.run())) #Starting the Heartbeat to show the Watchdog that the DPU is running
 
     mainTasks = []
     mainTasks.append(asyncio.create_task(getSensorData.DataScraper.collectData())) #Start collecting and saving sensor Data
+
+def bootLogic():
+    altitude = ms5637.ms5637.read()
+    
+    f = open('data/systemlog.txt','w') #opening the startAltitude text file in write mode
+    f.write(altitude) #writing current alitude to file
+    f.close() #closing file
+
