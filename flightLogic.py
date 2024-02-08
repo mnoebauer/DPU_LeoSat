c108afce185dbb64dc281d2849f751205b0a6465
Manuel <manuel2@noebauer.at>
2024-02-08 10:27:28 +0100

    tab fix

--- flightLogic.py
+++ flightLogic.py
@@ -7,12 +7,12 @@ def __main__():
     asyncio.run((mainFlightLogic()))
 
 async def mainFlightLogic():
-   """
-   This is the main logic running during the whole flight
-   It constantly runs the heartbeat to show the watchdog that it is alive
-   It constantly gets the sensor data and saves it
-   """
+    """
+    This is the main logic running during the whole flight
+    It constantly runs the heartbeat to show the watchdog that it is alive
+    It constantly gets the sensor data and saves it
+    """
 
-   mainTasks = []
-   mainTasks.append(asyncio.create_task(heartbeat.heartbeart.run())) #Starting the Heartbeat to show the Watchdog that the DPU is running
-   mainTasks.append(asyncio.create_task(getSensorData.DataScraper.collectData())) #Start collecting and saving sensor Data
+    mainTasks = []
+    mainTasks.append(asyncio.create_task(heartbeat.heartbeart.run())) #Starting the Heartbeat to show the Watchdog that the DPU is running
+    mainTasks.append(asyncio.create_task(getSensorData.DataScraper.collectData())) #Start collecting and saving sensor Data
