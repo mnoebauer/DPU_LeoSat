c1d4b5ec7c45a5fdcd39e8f30ebd542bd9571c6a
Manuel <manuel2@noebauer.at>
2024-02-08 13:46:13 +0100

    modified:   flightLogic.py

--- flightLogic.py
+++ flightLogic.py
@@ -8,6 +8,7 @@ def __main__():
 async def mainFlightLogic():
     """
     This is the main logic running during the whole flight
+    First it calls the boot logic and creates referance values
     It constantly runs the heartbeat to show the watchdog that it is alive
     It constantly gets the sensor data and saves it
     """
@@ -20,14 +21,14 @@ async def mainFlightLogic():
     mainTasks.append(asyncio.create_task(getSensorData.DataScraper.collectData())) #Start collecting and saving sensor Data
 
     #following loop runs constanly
-    
+    #TO DO while true loop for transmisson, height, etc...
 
 def bootLogic():
     altitude = ms5637.ms5637.read()
 
     #only writes it to the file if a boot/restart occurs under 1km, tp get a real reference height
     if altitude < 1000:
-        f = open('data/systemlog.txt','w') #opening the startAltitude text file in write mode
+        f = open('data/startAltitude.txt','w') #opening the startAltitude text file in write mode
         f.write(altitude) #writing current alitude to file
         f.close() #closing file
     
