58219e86a293becc3787db193668b61b5cfb76b6
Manuel <manuel2@noebauer.at>
2024-02-09 21:14:26 +0100

    modified:   Drivers/heartbeat.py

--- Drivers/heartbeat.py
+++ Drivers/heartbeat.py
@@ -6,10 +6,10 @@ class heartbeart:
     Driver class for the "Hearbeat" that sends a signal every 2 Minutes 
     to the Watchdog that the DPU is still alive
     """
-
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(17,GPIO.OUT, initial = GPIO.LOW)
     async def run(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(17,GPIO.OUT, initial = GPIO.LOW)
+
         while True:
             GPIO.output(17,1)
             await asyncio.sleep(0.5)
