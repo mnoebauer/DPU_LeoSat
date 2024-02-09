5cfbc726b5b5d3b10f11b784103ed86c4fefeddf
Manuel <manuel2@noebauer.at>
2024-02-09 20:55:33 +0100

    modified:   Drivers/rtc.py

--- Drivers/rtc.py
+++ Drivers/rtc.py
@@ -7,14 +7,12 @@ class RTC:
     Driver for the DS3231 RTC Module to get the time
     """
     i2c = board.I2C()
-    rtc = adafruit_ds3231(i2c)
+    rtc = adafruit_ds3231.DS3231(i2c)
 
     def read(self):
         r = self.rtc.datetime
         t = str(r.tm_hour) +":"+ str(r.tm_min)+":"+ str(r.tm_sec)
         return t
 
-    def set():
-        i2c = board.I2C()  
-        rtc = adafruit_ds3231.DS3231(i2c)   
-        rtc.datetime = time.struct_time((2024,0,0,0,0,0,0,1,-1)) #setting the hours to 0 to get the start time
+    #def set():  
+        #rtc.datetime = time.struct_time((2024,0,0,0,0,0,0,1,-1)) #setting the hours to 0 to get the start time
