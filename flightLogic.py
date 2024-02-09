45e9ec722ba9beeb71d556c8ea0769aa5800d964
Manuel <manuel2@noebauer.at>
2024-02-09 20:21:10 +0100

    modified:   flightLogic.py

--- flightLogic.py
+++ flightLogic.py
@@ -1,5 +1,7 @@
 import sys
 sys.path
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import asyncio
 from Drivers import heartbeat, ms5637, rtc
 import getSensorData
