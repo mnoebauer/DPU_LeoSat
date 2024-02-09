0051fe03b54c1a0bab56b65b12bcc7c0881aa52c
Manuel <manuel2@noebauer.at>
2024-02-09 21:17:07 +0100

    modified:   Drivers/heartbeat.py

--- Drivers/heartbeat.py
+++ Drivers/heartbeat.py
@@ -7,11 +7,11 @@ class heartbeart:
     to the Watchdog that the DPU is still alive
     """
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(17,GPIO.OUT, initial = GPIO.LOW)
+    GPIO.setup(11,GPIO.OUT)
     async def run(self):
 
         while True:
-            GPIO.output(17,1)
+            GPIO.output(11,1)
             await asyncio.sleep(0.5)
-            GPIO.output(17,0)
+            GPIO.output(11,0)
             await asyncio.sleep(120)
