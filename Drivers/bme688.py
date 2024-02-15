2cf5b7ebb6fe8d277048f3ca074f904e43720156
Manuel <manuel2@noebauer.at>
2024-02-15 10:52:46 +0100

    modified:   Drivers/bme688.py

--- Drivers/bme688.py
+++ Drivers/bme688.py
@@ -1,12 +1,12 @@
 import bme680
 
 class BME680:
-    def read():
-        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY) 
-        sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-
-        if sensor.get_sensor_data():
-            gas = sensor.data.gas_resistance
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY) 
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    
+    def read(self):
+        if self.sensor.get_sensor_data():
+            gas = self.sensor.data.gas_resistance
         else:
             gas = "NaN"
         return gas
