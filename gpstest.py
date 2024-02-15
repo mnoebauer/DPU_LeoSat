fea73486b0501a68d9b1bfc4f9459b49d2de6359
Manuel <manuel2@noebauer.at>
2024-02-15 12:02:17 +0100

    modified:   gpstest.py

--- gpstest.py
+++ gpstest.py
@@ -1,17 +1,16 @@
 import qwiic_titan_gps
+from time import sleep
 
+
+print("SparkFun GPS Breakout - XA1110!")
 qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
-qwiicGPS.begin()
 
+
+qwiicGPS.begin()
 while True:
     if qwiicGPS.get_nmea_data() is True:
-        lat = qwiicGPS.gnss_messages['Latitude']
-        long = qwiicGPS.gnss_messages['Longitude']
-        alt = qwiicGPS.gnss_messages['Altitude']
-    else:
-        print("not true")
-        lat = "NaN"
-        long = "NaN"
-        alt = "NaN"
-            
-    print(str(lat)+";"+(str(long)+";"+(str(alt)+";")))
+        print("Latitude: {}, Longitude: {}, Time: {}".format(
+            qwiicGPS.gnss_messages['Latitude'],
+            qwiicGPS.gnss_messages['Longitude'],
+            qwiicGPS.gnss_messages['Time']))
+    sleep(1)
