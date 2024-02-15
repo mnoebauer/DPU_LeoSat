280988654866232f0904ae30eb2f92173d29b534
Manuel <manuel2@noebauer.at>
2024-02-15 12:04:44 +0100

    modified:   gpstest.py

--- gpstest.py
+++ gpstest.py
@@ -1,9 +1,13 @@
 import qwiic_titan_gps
 from time import sleep
-
+import sys
 
 print("SparkFun GPS Breakout - XA1110!")
 qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
+
+if qwiicGPS.connected is False:
+    print("Could not connect to to the SparkFun GPS Unit. Double check that\
+            it's wired correctly.", file=sys.stderr)
 
 
 qwiicGPS.begin()
