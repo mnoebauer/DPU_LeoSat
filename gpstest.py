c2db887808c895f469c4014e2556579e8d605d35
Manuel <manuel2@noebauer.at>
2024-02-15 12:07:40 +0100

    modified:   gpstest.py

--- gpstest.py
+++ gpstest.py
@@ -2,19 +2,29 @@ import qwiic_titan_gps
 from time import sleep
 import sys
 
-print("SparkFun GPS Breakout - XA1110!")
-qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
+def run_example():
 
-if qwiicGPS.connected is False:
-    print("Could not connect to to the SparkFun GPS Unit. Double check that\
-            it's wired correctly.", file=sys.stderr)
+    print("SparkFun GPS Breakout - XA1110!")
+    qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
+
+    if qwiicGPS.connected is False:
+        print("Could not connect to to the SparkFun GPS Unit. Double check that\
+              it's wired correctly.", file=sys.stderr)
+        return
+
+    qwiicGPS.begin()
+
+    while True:
+        if qwiicGPS.get_nmea_data() is True:
+            for k,v in qwiicGPS.gnss_messages.items():
+                print(k, ":", v)
+
+        sleep(1)
 
 
-qwiicGPS.begin()
-while True:
-    if qwiicGPS.get_nmea_data() is True:
-        print("Latitude: {}, Longitude: {}, Time: {}".format(
-            qwiicGPS.gnss_messages['Latitude'],
-            qwiicGPS.gnss_messages['Longitude'],
-            qwiicGPS.gnss_messages['Time']))
-    sleep(1)
+if __name__ == '__main__':
+    try:
+        run_example()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("Ending Basic Example.")
+        sys.exit(0)
