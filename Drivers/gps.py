c6fad697b1302b7ec915fbeace659d19831bf933
Manuel <manuel2@noebauer.at>
2024-02-15 11:25:10 +0100

    modified:   Drivers/gps.py

--- Drivers/gps.py
+++ Drivers/gps.py
@@ -1,15 +1,15 @@
 import qwiic_titan_gps
 
 class GPS:
-    qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
-    qwiicGPS.begin()
+    def __init__(self) -> None:
+        qwiicGPS = qwiic_titan_gps.QwiicTitanGps()
+        qwiicGPS.begin()
 
     def read(self):
-
         if self.qwiicGPS.get_nmea_data() is True:
-                lat = self.qwiicGPS.gnss_messages['Latitude']
-                long = self.qwiicGPS.gnss_messages['Longitude']
-                alt = self.qwiicGPS.gnss_messages['Altitude']
+            lat = self.qwiicGPS.gnss_messages['Latitude']
+            long = self.qwiicGPS.gnss_messages['Longitude']
+            alt = self.qwiicGPS.gnss_messages['Altitude']
         else:
             print("not true")
 
