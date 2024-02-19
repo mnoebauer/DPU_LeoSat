0969515bc55b7d7950672c7f6e075bc140b02175
Manuel <manuel2@noebauer.at>
2024-02-19 15:51:47 +0100

    modified:   Drivers/ptc.py

--- Drivers/ptc.py
+++ Drivers/ptc.py
@@ -10,9 +10,12 @@ from adafruit_ads1x15.analog_in import AnalogIn
 # Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
+time.sleep(1)
+
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
+time.sleep(1)
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0)
 
