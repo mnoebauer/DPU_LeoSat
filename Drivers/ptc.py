b97249785d83d6a43a36c96d0de90e913feaa8d8
Manuel <manuel2@noebauer.at>
2024-02-19 16:34:53 +0100

    modified:   Drivers/ptc.py

--- Drivers/ptc.py
+++ Drivers/ptc.py
@@ -1,10 +1,12 @@
-import ads1110
-from machine import I2C
+from smbus import SMBus
+import time
 
-i2c = I2C(freq = 100000)
+i2c = SMBus(1)
 
-ads = ads1110.ADS1110()
+i2cadress = 0x48
+temp3 = bytearray(3)
 
-b = ads.read(ads1110._DR_15SPS,ads1110._PGA_1)
+i2c.readfrom_into(i2cadress,temp3)
+results, status = temp3[0] << 8 | temp3[1], temp3[2]
 
-print(str(b))
+print(str(results))
