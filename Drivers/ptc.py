349deab980fcd46c23bcc053a226decb1fb63dbb
Manuel <manuel2@noebauer.at>
2024-02-19 16:40:39 +0100

    deleted:    Drivers/ads1110.py 	modified:   Drivers/ptc.py

--- Drivers/ptc.py
+++ Drivers/ptc.py
@@ -10,5 +10,8 @@ temp3 = bytearray(3)
 i2c.readfrom_into(i2cadress,temp3)
 results, status = temp3[0] << 8 | temp3[1], temp3[2]
 
+temp = (results & 0xFFF)/16
+
 print(str(results))
 print(str(status))
+print(str(temp))
