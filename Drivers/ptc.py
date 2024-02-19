114bd6133cb35dcac3e32e452bb247329f3e7a80
Manuel <manuel2@noebauer.at>
2024-02-19 16:02:26 +0100

    modified:   Drivers/ptc.py

--- Drivers/ptc.py
+++ Drivers/ptc.py
@@ -4,5 +4,7 @@ import time
 i2c = SMBus(1)
 i2cadress = 0x48
 
-b = i2c.read_byte_data(i2cadress,0x01,)
+b = i2c.read_byte_data(i2cadress,0x01)
+y = i2c.read_byte_data(i2cadress,0x02)
 print(str(b))
+print(str(y))
