6d6f024393a6b0f9f94edfa8d6dd8b539963f239
Manuel <manuel2@noebauer.at>
2024-02-19 16:01:28 +0100

    modified:   Drivers/ptc.py

--- Drivers/ptc.py
+++ Drivers/ptc.py
@@ -1,29 +1,8 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+from smbus import SMBus
 import time
-import board
-import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = SMBus(1)
+i2cadress = 0x48
 
-time.sleep(1)
-
-# Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
-
-time.sleep(1)
-# Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
-
-# Create differential input between channel 0 and 1
-# chan = AnalogIn(ads, ADS.P0, ADS.P1)
-
-print("{:>5}\t{:>5}".format("raw", "v"))
-
-while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+b = i2c.read_byte_data(i2cadress,0x01,)
+print(str(b))
