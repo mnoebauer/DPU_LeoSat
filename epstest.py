db6813bca56a707ba8f8638353eee9b5c5def10b
Manuel <manuel2@noebauer.at>
2024-02-22 12:53:49 +0100

    modified:   epstest.py

--- epstest.py
+++ epstest.py
@@ -5,8 +5,10 @@ bus = SMBus(1) # indicates /dev/ic2-1
  
 numb = 1
  
-print ("Enter 1 for ON or 0 for OFF")
-while numb == 1:
-	print("in")
-	bus.write_byte(addr, 1) # switch it on
-	print("done")
+
+bus.write_byte(addr, 1) # switch it on
+print("done")
+
+while True:
+    b = bus.read_byte_data(addr,5)
+    print(str(b))
