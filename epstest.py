1b09d6249db2c924906ea2d5f987e82a2f74f69b
Manuel <manuel2@noebauer.at>
2024-02-22 11:42:27 +0100

    modified:   epstest.py

--- epstest.py
+++ epstest.py
@@ -2,9 +2,10 @@ import smbus2 as SMBus
 from time import sleep
 
 epsAdr = 0x80
-
+SMBus = SMBus(1)
 while True:
-    with SMBus(1) as bus:
+
+    with SMBus as bus:
         epsData = bus.read_i2c_block_data(epsAdr,0,5)
     
     print(str(epsData))
