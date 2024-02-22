714a3819a60459eed5bb3be16c91061b6bd73cb8
Manuel <manuel2@noebauer.at>
2024-02-22 12:14:31 +0100

    modified:   epstest.py

--- epstest.py
+++ epstest.py
@@ -1,5 +1,19 @@
-from smbus2 import SMBus
-bus = SMBus(1)
 
-b = bus.read_byte(44)
-print(b)
+from smbus import SMBus
+ 
+addr = 0x08 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
+ 
+numb = 1
+ 
+print ("Enter 1 for ON or 0 for OFF")
+while numb == 1:
+ 
+	ledstate = 1
+ 
+	if ledstate == "1":
+		bus.write_byte(addr, 0x1) # switch it on
+	elif ledstate == "0":
+		bus.write_byte(addr, 0x0) # switch it on
+	else:
+		numb = 0
