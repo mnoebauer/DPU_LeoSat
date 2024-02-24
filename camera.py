3d652c2715a700dfae701132b8a21b84664cc866
Manuel <manuel2@noebauer.at>
2024-02-24 11:27:57 +0100

    modified:   camera.py

--- camera.py
+++ camera.py
@@ -5,29 +5,30 @@ import asyncio
 i = 0
 
 class camclass():
+        
+    picam2 = Picamera2()
     
-    async def takePicture():
-        picam2 = Picamera2()
-        config = picam2.create_still_configuration(main={"size": (1920, 1080)})
-        picam2.configure(config)
-        picam2.start()
+    async def takePicture(self):
+        config = self.picam2.create_still_configuration(main={"size": (1920, 1080)})
+        self.picam2.configure(config)
+        self.picam2.start()
 
         while True:
-            picam2.capture_file("picture"+ str(i) + ".jpg")
+            self.picam2.capture_file("picture"+ str(i) + ".jpg")
             i += 1
             asyncio.sleep(10)
     
 
-    async def takeVideo():
-        picam2 = Picamera2()
-        video_config = picam2.create_video_configuration() 
-        picam2.configure(video_config) 
+    async def takeVideo(self):
+
+        video_config = self.picam2.create_video_configuration() 
+        self.picam2.configure(video_config) 
         
         encoder = H264Encoder(bitrate=10000000) 
         output = "test.h264" 
-        picam2.start_recording(encoder, output) 
+        self.picam2.start_recording(encoder, output) 
         asyncio.sleep(10)
-        picam2.stop_recording()
+        self.picam2.stop_recording()
 
 
 camclassObj = camclass()
