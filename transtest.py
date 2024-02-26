1c73949fa38561b3384353b7052d12dc37ab82f1
Manuel <manuel2@noebauer.at>
2024-02-26 14:46:08 +0100

    new file:   transtest.py

---
+++ transtest.py
@@ -0,0 +1,11 @@
+import wiringpi
+from time import sleep
+
+s = "testtest"
+
+wiringpi.wiringPiSetup()
+serial = wiringpi.serialOpen('/dev/ttyAMA0',115200) #115200 Baudrate
+
+while True:
+    wiringpi.serialPuts(serial,s) #sends the selected row to com pcb
+    sleep(2)
