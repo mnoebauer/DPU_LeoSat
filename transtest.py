46ff9f6a5c5c27e3c8351ad8c869da7564d34e04
Manuel <manuel2@noebauer.at>
2024-02-26 18:19:04 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,6 +1,17 @@
-import wiringpi
-wiringpi.wiringPiSetup()
-serial = wiringpi.serialOpen('/dev/ttAMA0',baud = 9600)
+import time
+import serial
 
-while True:
-    wiringpi.serialPuts(serial,'1')
+ser = serial.Serial(
+        port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+counter=0
+
+while 1:
+        ser.write("Write counter: %d \n"%(counter))
+        time.sleep(1)
+        counter += 1
