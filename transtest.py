ccb4d9f4ba78e482c8f416d8991c07d95762fc8c
Manuel <manuel2@noebauer.at>
2024-02-26 15:22:16 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,12 +1,6 @@
 import wiringpi
-from time import sleep
-
-s = "testtest"
-
 wiringpi.wiringPiSetup()
-serial = wiringpi.serialOpen('/dev/ttyAMA0',115200) #115200 Baudrate
+serial = wiringpi.serialOpen('/dev/ttyAMA0',115200)
 
 while True:
-    wiringpi.serialPuts(serial,'hell world') #sends the selected row to com pcb
-    print("sent")
-    sleep(1)
+    wiringpi.serialPuts(serial,'hello world!')
