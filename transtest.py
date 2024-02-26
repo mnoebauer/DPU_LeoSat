34fdbd98b8d048cbdea3d35fa696d2b3cedca9b8
Manuel <manuel2@noebauer.at>
2024-02-26 17:40:00 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,6 +1,6 @@
 import wiringpi
 wiringpi.wiringPiSetup()
-serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
+serial = wiringpi.serialOpen('/dev/serial0,9600')
 
 while True:
     wiringpi.serialPuts(serial,'1')
