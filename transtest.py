c57da4302060704ec542ddacae96488e75217f79
Manuel <manuel2@noebauer.at>
2024-02-26 15:37:21 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,6 +1,23 @@
-import wiringpi
+'''import wiringpi
 wiringpi.wiringPiSetup()
 serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
 
 while True:
     wiringpi.serialPuts(serial,'hello world!')
+'''
+#!/usr/bin/env python
+import time
+import serial
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+while 1:
+        x=ser.readline()
+        print x
