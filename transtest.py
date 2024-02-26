bcd9f320a66bf4c81382b99f7478e570a43079b6
Manuel <manuel2@noebauer.at>
2024-02-26 14:54:40 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -8,4 +8,5 @@ serial = wiringpi.serialOpen('/dev/ttyAMA0',115200) #115200 Baudrate
 
 while True:
     wiringpi.serialPuts(serial,s) #sends the selected row to com pcb
+    print("sent")
     sleep(2)
