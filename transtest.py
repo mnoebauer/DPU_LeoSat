ca6771c708dad51e8ac2013fd44d538a2b85dbf3
Manuel <manuel2@noebauer.at>
2024-02-26 15:52:39 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,27 +1,6 @@
-'''import wiringpi
+import wiringpi
 wiringpi.wiringPiSetup()
 serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
 
 while True:
     wiringpi.serialPuts(serial,'hello world!')
-'''
-#!/usr/bin/env python
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import serial
-test_string = "Test serial port ...".encode('utf-8')
-port_list = ["/dev/ttyAMA0","/dev/ttyAMA0","/dev/ttyS0","/dev/ttyS"]
-for port in port_list:
-  try:
-    serialPort = serial.Serial(port, 9600, timeout = 2)
-    print ("Serial port", port, " ready for test :")
-    bytes_sent = serialPort.write(test_string)
-    print ("Sended", bytes_sent, "byte")
-    loopback = serialPort.read(bytes_sent)
-    if loopback == test_string:
-      print ("Received ",len(loopback), "bytes. Port", port,"is OK ! \n")
-    else:
-      print ("Received incorrect data:", loopback, "on serial part", port, "loopback \n")
-    serialPort.close()
-  except IOError:
-    print ("Error on", port,"\n")
