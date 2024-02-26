a9dd74dfe4b8b3f7e0ccd9c7e07d9382ba80e895
Manuel <manuel2@noebauer.at>
2024-02-26 15:29:46 +0100

    modified:   transtest.py

--- transtest.py
+++ transtest.py
@@ -1,17 +1,6 @@
-'''import wiringpi
+import wiringpi
 wiringpi.wiringPiSetup()
-serial = wiringpi.serialOpen('/dev/ttyAMA0',115200)
+serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
 
 while True:
     wiringpi.serialPuts(serial,'hello world!')
-'''
-
-import serial
-from time import sleep
-while True:
-    ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
-    ser.baudrate = 115200                     #Set baud rate to 9600
-    data = 74                 #Read ten characters from serial port to data
-    ser.write(data)                         #Send back the received data
-    ser.close()        
-    sleep(1)
